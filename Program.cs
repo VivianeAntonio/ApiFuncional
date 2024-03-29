78f4f7b2631c005d2d7c7d6824d0c2d11ea6d2ac
VivianeAntonio <asilvaviviane@gmail.com>
2024-03-29 06:54:48 -0300

    Inclusão de Identity no projeto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ApiFuncional.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,10 @@ builder.Services.AddDbContext<ApiDbContext>( options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApiDbContext>();
+    
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -22,6 +27,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
